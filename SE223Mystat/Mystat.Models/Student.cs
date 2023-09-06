eaa0e149f93e839d382054c2e26e0757ce56d372
Admin <Admin@T-25-01>
2023-09-06 21:58:10 +0400

    Student model added

---
+++ SE223Mystat/Mystat.Models/Student.cs
@@ -0,0 +1,15 @@
+﻿namespace Mystat.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get; set; }
+        public DateTime RegisterDate { get; set; }
+        public bool Attends { get; set; }
+        public bool AttendsOnline { get; set; }
+        public int Brilliants { get; set; }
+        public string Comment { get; set; }
+    }
+}
