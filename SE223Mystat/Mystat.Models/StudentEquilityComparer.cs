d80a98041e25eba9d5c877ff9ac592923c555154
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2023-09-10 22:07:09 +0400

    Equility comparer added for students

---
+++ SE223Mystat/Mystat.Models/StudentEquilityComparer.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Mystat.Models
+{
+    public class StudentEquilityComparer : IEqualityComparer<Student>
+    {
+        public bool Equals(Student x, Student y)
+        {
+            return x.Id == y.Id && x.FullName == y.FullName;
+        }
+
+        public int GetHashCode([DisallowNull] Student obj)
+        {
+            return obj.Id;
+        }
+    }
+}
