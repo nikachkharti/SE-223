9134ce8b6478d83877db162eb9fecafea593a317
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2023-09-10 22:07:57 +0400

    Tests project added

---
+++ SE223Mystat/Mystat.Tests/GlobalUsings.cs
@@ -0,0 +1,3 @@
+global using Xunit;
+global using Mystat.Models;
+global using MystatService;
---
+++ SE223Mystat/Mystat.Tests/Student_Service_Should.cs
@@ -0,0 +1,55 @@
+﻿using MystatService.Interfaces;
+
+namespace MystatService
+{
+    public class Student_Service_Should
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public Student_Service_Should()
+        {
+            _unitOfWork = new UnitOfWork();
+        }
+
+        [Fact]
+        public void Return_All_Students_From_Database()
+        {
+            //var actual = _unitOfWork.Student.GetAllStudents();
+
+            //var expected = new List<Student>()
+            //{
+            //    new Student()
+            //    {
+            //        Id = 1,
+            //        FirstName = "ნიკოლოზ",
+            //        LastName = "ლაშხია",
+            //        RegisterDate = new DateTime(2023,09,10,20,43,13,827),
+            //        FullName = "ნიკოლოზლაშხია",
+            //        Attends = true,
+            //        AttendsOnline = true,
+            //        Brilliants = 10,
+            //        Comment = null
+            //    }
+            //};
+
+
+            //Assert.Equal(expected, actual, new StudentEquilityComparer());
+        }
+
+        [Fact]
+        public void Add_New_Student_In_Database()
+        {
+            //Student newStudent = new()
+            //{
+            //    FirstName = "რატი",
+            //    LastName = "მამულაშვილი",
+            //    Attends = true,
+            //    AttendsOnline = true,
+            //    Brilliants = 10,
+            //    Comment = string.Empty
+            //};
+
+            //_unitOfWork.Student.AddNewStudent(newStudent);
+        }
+
+    }
+}
