127f1bc755e93fbdc84c81c21639aee378af2ca5
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2023-09-24 20:02:44 +0400

    Old project removed

--- SE223Mystat/Mystat.Abstraction/HelperConfig.cs
+++ SE223Mystat/Mystat.Abstraction/HelperConfig.cs
@@ -2,6 +2,6 @@
 {
     public static class HelperConfig
     {
-        public static string ConnectionString { get; } = @"Server=T-25-01\SQLEXPRESS;Database=SE223;Trusted_Connection=True;TrustServerCertificate=True;MultipleActiveResultSets=True";
+        public static string ConnectionString { get; } = @"Server=DESKTOP-SCSHELD\SQLEXPRESS;Database=SE223;Trusted_Connection=True;TrustServerCertificate=True;MultipleActiveResultSets=True";
     }
 }
