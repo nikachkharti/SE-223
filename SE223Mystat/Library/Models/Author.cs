2a7fc44849643541c303125ddc271040c4b8818a
Admin <Admin@T-25-01>
2023-09-27 21:58:18 +0400

    Books project added

---
+++ SE223Mystat/Library/Models/Author.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library.Models
+{
+    public class Author
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string? FirstName { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string? LastName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public DateTime? DateOfDeath { get; set; }
+    }
+}
---
+++ SE223Mystat/Library/Models/BookAuthor.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Library.Models
+{
+    public class BookAuthor
+    {
+        public int BookId { get; set; }
+        public int AuthorId { get; set; }
+        [ForeignKey("BookId")]
+        public Book? Book { get; set; }
+        [ForeignKey("AuthorId")]
+        public Author? Author { get; set; }
+    }
+}
