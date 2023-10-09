981759371325ef4e5bfa929dacc5e791890cb655
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2023-10-09 16:51:10 +0400

    Create added

--- SE223Mystat/Library/Controllers/BooksController.cs
+++ SE223Mystat/Library/Controllers/BooksController.cs
@@ -34,7 +34,18 @@ namespace Library.Controllers
 
         public IActionResult Create()
         {
-            return View();
+            BookWithManyAuthors bookWithManyAuthors = new();
+            bookWithManyAuthors.Book = new();
+            bookWithManyAuthors.Authors = _context.Authors.ToList();
+
+            //ძველი კოდი არ წავშალე სპეციალურად თუ დაგჭირდებათ ჩმოსაშლელი მენიუს გამოყენება ეწყობა ესე.
+            //bookWithManyAuthors.AuthorsSelectList = bookWithManyAuthors.Authors.Select(x => new SelectListItem()
+            //{
+            //    Text = $"{x.FirstName} {x.LastName}",
+            //    Value = x.Id.ToString()
+            //});
+
+            return View(bookWithManyAuthors);
         }
 
         public IActionResult Delete(int id)
@@ -56,11 +67,33 @@ namespace Library.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create(Book book)
+        public IActionResult Create(BookWithManyAuthors newBookModel)
         {
             if (ModelState.IsValid)
             {
-                _context.Books.Add(book);
+                _context.Books.Add(newBookModel.Book);
+                _context.SaveChanges();
+
+                foreach (var selectedAuthorIdAsText in newBookModel.SelectedAuthors)
+                {
+                    int.TryParse(selectedAuthorIdAsText, out int selectedAuthorID);
+
+                    var author = _context.Authors.Find(selectedAuthorID);
+
+                    if (author != null)
+                    {
+                        _context.BookAuthor.Add(new BookAuthor
+                        {
+                            BookId = newBookModel.Book.Id,
+                            AuthorId = selectedAuthorID
+                        });
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index");
+                    }
+                }
+
                 _context.SaveChanges();
             }
             return RedirectToAction("Index");
---
+++ SE223Mystat/Library/Models/BookWithManyAuthors.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Library.Models
+{
+    public class BookWithManyAuthors
+    {
+        public Book? Book { get; set; }
+        public List<Author>? Authors { get; set; }
+        //ძველი კოდი არ წავშალე სპეციალურად თუ დაგჭირდებათ ჩმოსაშლელი მენიუს გამოყენება ეწყობა ესე.
+        //public IEnumerable<SelectListItem>? AuthorsSelectList { get; set; }
+        public List<string>? SelectedAuthors { get; set; }
+    }
+}
